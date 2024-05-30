ace6626845addbdc06409c4224875e5e1b1d42c1
Shantanu <104728982+ShaanKeshri@users.noreply.github.com>
2024-05-30 13:48:37 +0530

    kash router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 // eslint-disable-next-line no-unused-vars
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Navbarc from './components/Navbar';
 import AllRoutes from './AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Navbarc />
         <AllRoutes />
-      </Router>
+      </HashRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware } from 'redux';
+// import { configureStore, compose, applyMiddleware } from 'redux';
 
-// import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk';
 import Reducers from './reducers/index';
 
-const store = createStore(Reducers, compose(applyMiddleware(thunk)));
-// const store = configureStore({
-//   reducer: Reducers,
-//   Middleware: [thunk],
-// });
+// const store = configureStore(Reducers, compose(applyMiddleware(thunk)));
+const store = configureStore({
+  reducer: Reducers,
+  Middleware: [thunk],
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
